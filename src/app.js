3711a2e75a3c9166126243e812ef7cf9979495f3
brunaoflofl <bruna.oflima@gmail.com>
2019-09-26 14:48:08 -0300

    progeto inicial react+webpack

--- src/app.js
+++ src/app.js
@@ -1,81 +1,8 @@
 'use strict'
 
-import React, {Component} from 'react'
-import ajax from '@fdaciuk/ajax'
-import AppContent from './components/app-content'
+import React from 'react'
 
-
-//marcação completa da aplicação
-class App extends Component  {
-    constructor(){
-        super()
-        this.state = {
-            userinfo: null,
-            repos: [],
-            starred: []
-          
-            }
-
-        }
-
-        getGitHubApiUrl (username, type) {
-            const internalUser = username ? `/${username}` : ''
-            const internalType = type ? `/${type}` : ''
-            return `https://api.github.com/users${internalUser}${internalType}`
-        }
-    
-
-    handleSearch(e) {
-        const value = e.target.value
-        const keyCode = e.which || e.keyCode
-        const ENTER = 13
-
-        if(keyCode === ENTER) {
-            ajax().get(this.getGitHubApiUrl(value))
-            .then((result) => {
-                this.setState({
-                    userinfo: {
-                        username: result.name,
-                        photo: result.avatar_url,
-                        login: result.login,
-                        repos: result.public_repos,
-                        followers: result.followers,
-                        following: result.following
-                    },
-                    repos: [],
-                    starred: []
-                })
-            })
-        }
-    }
-
-    getRepos (type) {
-        return (e) => {
-            const username = this.state.userinfo.login
-            ajax().get(this.getGitHubApiUrl(username, type))
-                .then((result) => {
-                    this.setState({
-                        [type]: result.map((repo) => ({ 
-                                name: repo.name,
-                                link: repo.html_url
-
-                            }))
-                        })
-                    })
-                
-        }
-    }
-
-    render(){
-        return <AppContent
-            userinfo={this.state.userinfo}
-            repos={this.state.repos}
-            starred={this.state.starred}
-            handleSearch={(e) => this.handleSearch(e)}
-            getRepos={this.getRepos('repos')}
-            getStarred={this.getRepos('starred')}
-        />
-    }
-}
-
+const App = () => (
+    <div>My App</div>
+)
 export default App
