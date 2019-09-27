f1bf4f7e617ec353e87a579271c4bf577b98eb2b
brunaoflofl <bruna.oflima@gmail.com>
2019-09-27 13:36:39 -0300

    testes unitários em componentes do lado do servidor

---
+++ src/title.js
@@ -0,0 +1,9 @@
+'use strict'
+//teste unitario: para cada parte do codigo um teste
+
+const React = require('react')
+
+const Title = () => (
+   React.createElement('h1', {}, 'Title')
+)
+module.exports = Title
